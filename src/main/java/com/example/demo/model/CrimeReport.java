502ffc3c206b98adcaecd8229adc37d90591b324
bala-amypo <bala@amypo.in>
2025-12-18 09:12:46 +0000

    User commit

--- src/main/java/com/example/demo/model/CrimeReport.java
+++ src/main/java/com/example/demo/model/CrimeReport.java
@@ -1,4 +1,64 @@
-<dependency>
-<groupId>org.springframework.boot</groupId>
-<artifactId>spring-boot-starter-validation</artifactId>
-</dependency>
+package com.example.demo.model;
+
+import jakarta.persistence.Id;
+
+public class CrimeReport{
+    @Id
+    private Long id;
+    private String crimeType;
+    private String description;
+    @Size(min= -90,max=90)
+    private Double latitude;
+    @Si
+    private Double longitude;
+    private LocalDateTime occuredAt;
+
+     public CrimeReport(Long id, String crimeType, String description, Double latitude, Double longitude,
+            LocalDateTime occuredAt) {
+        this.id = id;
+        this.crimeType = crimeType;
+        this.description = description;
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.occuredAt = occuredAt;
+    }
+    public CrimeReport() {
+    }
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getCrimeType() {
+        return crimeType;
+    }
+    public void setCrimeType(String crimeType) {
+        this.crimeType = crimeType;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    public Double getLatitude() {
+        return latitude;
+    }
+    public void setLatitude(Double latitude) {
+        this.latitude = latitude;
+    }
+    public Double getLongitude() {
+        return longitude;
+    }
+    public void setLongitude(Double longitude) {
+        this.longitude = longitude;
+    }
+    public LocalDateTime getOccuredAt() {
+        return occuredAt;
+    }
+    public void setOccuredAt(LocalDateTime occuredAt) {
+        this.occuredAt = occuredAt;
+    }
+
+}
