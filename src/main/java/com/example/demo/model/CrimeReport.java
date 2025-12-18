11ed5c4184420d027a3165571514a285e94ffe71
bala-amypo <bala@amypo.in>
2025-12-18 08:47:43 +0000

    User commit

---
+++ src/main/java/com/example/demo/model/CrimeReport.java
@@ -0,0 +1,10 @@
+package com.example.demo.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Id;
+
+public class CrimeReport{
+    private Long id;
+    private String crimeType;
+    
+}
