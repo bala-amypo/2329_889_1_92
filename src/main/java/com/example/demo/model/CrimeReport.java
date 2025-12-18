7305dcf7aac05451cd390eac222a404ea8ac2194
bala-amypo <bala@amypo.in>
2025-12-18 09:02:46 +0000

    User commit

--- src/main/java/com/example/demo/model/CrimeReport.java
+++ src/main/java/com/example/demo/model/CrimeReport.java
@@ -1,13 +1,4 @@
-package com.example.demo.model;
-
-import jakarta.persistence.Id;
-
-public class CrimeReport{
-    @Id
-    private Long id;
-    private String crimeType;
-    private String description;
-    private Double latitude;
-    private Double longitude;
-    private LocalDateTime occuredAt;
-}
+<dependency>
+<groupId>org.springframework.boot</groupId>
+<artifactId>spring-boot-starter-validation</artifactId>
+</dependency>
