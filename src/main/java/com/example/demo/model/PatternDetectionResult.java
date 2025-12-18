6b8979bab605fbebd6cfb234c9921676741ca0f5
bala-amypo <bala@amypo.in>
2025-12-18 19:21:30 +0000

    User commit

--- src/main/java/com/example/demo/model/PatternDetectionResult.java
+++ src/main/java/com/example/demo/model/PatternDetectionResult.java
@@ -1,3 +1,10 @@
 package com.example.demo.model;
 
 import jakarta.persistence.Id;
+
+public class PattrenDetectionResult{
+    private Long Id;
+    private LocalDate analysisDate;
+    private integer crimeCount;
+
+}
