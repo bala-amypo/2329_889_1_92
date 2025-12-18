e7e172fd338a0b925beedddb886acb636d08cf3b
bala-amypo <bala@amypo.in>
2025-12-18 19:27:30 +0000

    User commit

--- src/main/java/com/example/demo/model/HotspotZone.java
+++ src/main/java/com/example/demo/model/HotspotZone.java
@@ -2,6 +2,7 @@ package com.example.demo.model;
 import jakarta.persistence.Id;
 
 public class HotspotZone{
+    @Id;
     private Long id;
     private String zoneName;
     private Double centerLat;
