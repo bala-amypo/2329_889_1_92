ad9323a7e2f8c8f4a93f157d3ee24c3a23752f8e
bala-amypo <bala@amypo.in>
2025-12-26 18:20:55 +0000

    User commit

--- src/main/java/com/example/demo/model/CrimeReport.java
+++ src/main/java/com/example/demo/model/CrimeReport.java
@@ -30,5 +30,51 @@ public class CrimeReport {
         this.occurredAt = occurredAt;
     }
 
-    // getters and setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCrimeType() {
+        return crimeType;
+    }
+
+    public void setCrimeType(String crimeType) {
+        this.crimeType = crimeType;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(Double latitude) {
+        this.latitude = latitude;
+    }
+
+    public Double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(Double longitude) {
+        this.longitude = longitude;
+    }
+
+    public LocalDateTime getOccurredAt() {
+        return occurredAt;
+    }
+
+    public void setOccurredAt(LocalDateTime occurredAt) {
+        this.occurredAt = occurredAt;
+    }
 }
